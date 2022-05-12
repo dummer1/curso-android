b289b3d4750ce8b6526225aa254b46de85fb0f08
Rafael Menezes <rafaelchmenezes@protonmail.com>
2022-05-12 20:18:42 -0300

    adicionando resolucao 7-45

---
+++ src/main/kotlin/7-45.kt
@@ -0,0 +1,30 @@
+/*
+1- Imprima os números de 1 a 50 na mesma linha em ordem crescente. Saída esperada:
+“1 2 3 4 5 6 7 8 9 10 .. .. .. 49 50”
+2- Imprima os números de 1 a 50 na mesma linha em ordem decrescente. Saída esperada:
+“50 49 48 47 .. .. .. 2 1”
+3- Usando a resolução do exercício 1, não imprima os números múltiplos de 5.
+4- Faça a soma de todos os números no intervalo de 1 a 500.
+5- Escreva um programa capaz de receber um número inteiro N e imprimir uma escada de tamanho N
+usando o caracter #.
+Se a entrada do programa for 3, teremos:
+#
+##
+###
+Se a entrada do programa for 5, teremos:
+#
+##
+###
+####
+#####
+*/
+
+fun main() {
+   //exercicio1()
+}
+
+fun exercicio1() {
+    for (number in 1..50) {
+        print("$number ")
+    }
+}
